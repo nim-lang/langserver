22d0f2ebd5362dc4c6e1ebb555e0260404bd18dd
Ivan Yonchovski <yyoncho@gmail.com>
2022-01-18 09:32:02 +0200

    WIP

--- nls.nim
+++ nls.nim
@@ -1,3 +1,13 @@
 import
-  json, os, streams, asyncdispatch, locks,
+  json, os, streams, asyncdispatch, locks, asynctools/asyncpipe,
   langserver/[base_protocol, commands]
+
+
+import
+  json_rpc/streamconnection
+
+let input = asyncWrap(stdout.getFileHandle(), stdin.getFileHandle());
+
+let connection = StreamConnection.new(input, input);
+
+waitFor connection.start();
