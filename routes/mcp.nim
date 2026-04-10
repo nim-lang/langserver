da3b4481de8c76e4f9e70dc3d3ae3daa9d714c3e
Constantine Molchanov <moigagoo@duck.com>
2026-04-10 16:52:26 +0400

    Add routes for tool listing and calling.
    nimFindReferences is a functioning route. nimExpandMacro and
    nimDiagnostics are placeholder routes.

--- routes/mcp.nim
+++ routes/mcp.nim
@@ -28,7 +28,7 @@ proc logToFile(msg: string) =
   logFile.writeLine(msg)
   close(logFile)
 
-#routes
+# Routes
 proc initialize*(
     p: tuple[ls: LanguageServer, onExit: OnExitCallback], params: McpInitializeParams
 ): Future[McpInitializeResult] {.async.} =
@@ -56,6 +56,7 @@ proc initialize*(
     capabilities: McpServerCapabilities(tools: some(McpToolsOptions())),
     serverInfo: McpInitializeParams_serverInfo(name: "nimlangserver", version: "1.12.0"),
   )
+  logToFile "result = " & $(%*result)
 
   debug "Initialize completed. Trying to start nimsuggest instances"
   logToFile "Initialize completed. Trying to start nimsuggest instances"
@@ -77,3 +78,187 @@ proc initialize*(
         logToFile "Starting nimsuggest for entry point " & entryPoint
         if entryPoint notin ls.projectFiles:
           ls.createOrRestartNimsuggest(entryPoint)
+
+proc listTools*(
+    ls: LanguageServer, params: McpListToolsParams
+): Future[McpListToolsResult] {.async.} =
+  logToFile "--== List tools started ==--"
+  logToFile "params = " & $(%*params)
+
+  result = McpListToolsResult(
+    tools: @[
+      McpTool(
+        name: "nimFindReferences",
+        title: some "Find symbol references in .nim files",
+        description:
+          some "Find references of the symbol under cursor in the current workspace.",
+        inputSchema: McpToolSchema(
+          `type`: "object",
+          properties: some %*{
+            "path": {"type": "string"},
+            "line": {"type": "integer"},
+            "column": {"type": "integer"},
+          },
+          required: some @["path", "line", "column"],
+        ),
+        outputSchema: some McpToolSchema(
+          `type`: "object",
+          properties: some %*{
+            "refs": {
+              "type": "array",
+              "items": {
+                "type": "object",
+                "properties": {
+                  "path": {"type": "string"},
+                  "line": {"type": "integer"},
+                  "column": {"type": "integer"},
+                },
+                "required": ["path", "line", "column"],
+              },
+            }
+          },
+          required: some @["refs"],
+        ),
+      ),
+      McpTool(
+        name: "nimExpandMacro",
+        description: some "Expand macro under cursor.",
+        inputSchema: McpToolSchema(
+          `type`: "object",
+          properties: some %*{
+            "path": {"type": "string"},
+            "line": {"type": "integer"},
+            "column": {"type": "integer"},
+          },
+          required: some @["path", "line", "column"],
+        ),
+      ),
+      McpTool(
+        name: "nimDiagnostics",
+        description:
+          some "Get diagnostics for a .nim file, i.e. errors, warnings, hints.",
+        inputSchema: McpToolSchema(
+          `type`: "object",
+          properties: some %*{"path": {"type": "string"}},
+          required: some @["path"],
+        ),
+      ),
+    ]
+  )
+
+  logToFile "result = " & $(%*result)
+
+  logToFile "List tools completed"
+
+proc callTool*(
+    ls: LanguageServer, params: McpCallToolParams
+): Future[McpCallToolResult] {.async.} =
+  logToFile "--== Call tool started ==--"
+
+  let arguments = params.arguments.get(JsonNode())
+
+  logToFile "params.name = " & params.name
+  logToFile "arguments = " & $arguments
+
+  result =
+    case params.name
+    of "nimFindReferences":
+      let
+        path = arguments["path"].getStr()
+        uri = path.pathToUri()
+        line = arguments["line"].getInt()
+        column = arguments["column"].getInt()
+
+      if uri notin ls.openFiles:
+        await ls.didOpenFile(
+          TextDocumentItem(
+            uri: uri, languageId: "nim", version: 0, text: readFile(path)
+          )
+        )
+
+      let nimsuggest = await ls.tryGetNimsuggest(uri)
+
+      if nimsuggest.isSome:
+        logToFile "nimsuggestPath = " & nimsuggest.get.nimsuggestPath
+        logToFile "project.file = " & nimsuggest.get.project.file
+
+        let references = await nimsuggest.get.use(path, path, line, column)
+
+        var usageReferencesJson = newJArray()
+
+        for reference in references:
+          if reference.section == ideUse:
+            usageReferencesJson.add %*{
+              "path": reference.filePath,
+              "line": reference.line,
+              "column": reference.column,
+            }
+
+        McpCallToolResult(
+          content: @[McpContentBlock(`type`: TextContent, text: $usageReferencesJson)],
+          structuredContent: some usageReferencesJson,
+          isError: some false,
+        )
+      else:
+        McpCallToolResult(
+          content:
+            @[McpContentBlock(`type`: TextContent, text: "Nimsuggest is unavailable")],
+          isError: some true,
+        )
+    of "nimExpandMacro":
+      let
+        path = arguments["path"].getStr()
+        uri = path.pathToUri()
+        line = arguments["line"].getInt()
+        column = arguments["column"].getInt()
+        text = readFile(path)
+
+      await ls.didOpenFile(
+        TextDocumentItem(uri: uri, languageId: "nim", version: 0, text: text)
+      )
+
+      let nimsuggest = await ls.tryGetNimsuggest(uri)
+
+      if nimsuggest.isSome:
+        let expanded =
+          await nimsuggest.get.expand(path, ls.uriToStash(uri), line, column)
+
+        if expanded.len > 0 and expanded[0].doc != "":
+          let expandedMacro = expanded[0].doc
+          logToFile("expandedMacro = " & expandedMacro)
+
+          McpCallToolResult(
+            content: @[McpContentBlock(`type`: TextContent, text: expandedMacro)],
+            isError: some false,
+          )
+        else:
+          McpCallToolResult(
+            content:
+              @[McpContentBlock(`type`: TextContent, text: "Could not expand macro")],
+            isError: some true,
+          )
+      else:
+        McpCallToolResult(
+          content:
+            @[McpContentBlock(`type`: TextContent, text: "Nimsuggest is unavailable")],
+          isError: some true,
+        )
+    of "nimDiagnostics":
+      McpCallToolResult(
+        content: @[McpContentBlock(`type`: TextContent, text: "Here be diagnostics")],
+        isError: some false,
+      )
+    else:
+      McpCallToolResult(
+        content: @[McpContentBlock(`type`: TextContent, text: "Unknown tool")],
+        isError: some true,
+      )
+
+  logToFile "result = " & $(%*result)
+
+# Notifications
+proc initialized*(ls: LanguageServer, _: JsonNode) {.async.} =
+  debug "Client initialized."
+  logToFile "--== Client initialized ==--"
+  # maybeRegisterCapabilityDidChangeConfiguration(ls)
+  # maybeRequestConfigurationFromClient(ls)
