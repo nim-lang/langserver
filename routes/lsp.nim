6963a0678df1d41993e2f95beebdef1672a1207e
Esteban C Borsani <ecastroborsani@gmail.com>
2026-09-18 08:41:10 -0300

    Fix `executeCommand` crashes (#463)

--- routes/lsp.nim
+++ routes/lsp.nim
@@ -768,6 +768,11 @@ proc executeCommand*(
     ls: LanguageServer, params: ExecuteCommandParams
 ): Future[JsonNode] {.async: (raises: [ApplicationError, CancelledError]).} =
   ls.checkInitialized()
+  if params.arguments.len == 0:
+    raise (ref ApplicationError)(
+      code: ErrorCode.InvalidParams.int,
+      msg: params.command & " expects the project file as its first argument",
+    )
   let projectFile = params.arguments[0].getStr
   case params.command
   of RESTART_COMMAND:
@@ -780,12 +785,12 @@ proc executeCommand*(
     debug "Clean build", projectFile = projectFile
     let
       token = fmt "Compiling {projectFile}"
-      ns = ls.projectFiles.getOrDefault(projectFile).ns
-    if ns != nil:
+      project = ls.projectFiles.getOrDefault(projectFile)
+    if project != nil and project.ns != nil:
       ls.workDoneProgressCreate(token)
       ls.progress(token, "begin", fmt "Compiling project {projectFile}")
 
-      ns.await().recompile().addCallback do(data: pointer):
+      project.ns.await().recompile().addCallback do(data: pointer):
         ls.progress(token, "end")
         ls.checkProject(projectFile.pathToUri).traceAsyncErrors
 
